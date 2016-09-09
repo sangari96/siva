446114cf1f2c6c5d5c8492dd1be995eeb3a470d0
sangari96 <shivashankari643@gmail.com>
2016-09-09 17:51:58 +0530

    Create v.c

---
+++ v.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+ 
+
+void reverse(char *str)
+{
+   if (*str)
+   {
+       reverse(str+1);
+       printf("%c", *str);
+   }
+}
+ 
+/* Driver program to test above function */
+int main()
+{
+   char a[] = "Geeks for Geeks";
+   reverse(a);
+   return 0;
+}
